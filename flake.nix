c724ccb92091c95e79e9e37f891834f21ca8ce44
James Benze <james@benze.me>
2024-02-29 14:24:22 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay }: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ rust-overlay.overlays.default ];
+    };
+  in
+  {
+    devShell.${system} = pkgs.mkShell {
+      packages = with pkgs; [
+        pkgs.rust-bin.stable.latest.default
+      ];
+    };
+  };
+}
